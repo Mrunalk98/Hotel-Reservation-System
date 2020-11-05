afc883b54b8dbef70cba83ca761cee9d83f8d0d1
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-11-05 22:53:59 +0530

    [mrunal] Add. Added welcome message in main method

---
+++ HotelReservationSystemProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HotelReservationSystemProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hotel Reservation System Program!");
+        }
+    }
+}

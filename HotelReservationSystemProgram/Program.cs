6261c9e38343af421fbd3add1f3f90b7c8dcd34b
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-11-08 00:28:21 +0530

    [mrunal] Add. Added method to validate date inputs

--- HotelReservationSystemProgram/Program.cs
+++ HotelReservationSystemProgram/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace HotelReservationSystemProgram
 {
@@ -18,7 +19,30 @@ namespace HotelReservationSystemProgram
             reservation.AddHotel(5, "Ridgewood", 220, 150, REGULAR, 5);
             reservation.AddHotel(6, "Ridgewood", 100, 40, REWARD, 5);
             reservation.DisplayHotels();
-            reservation.GetCheapestBestRatedHotel(REWARD, DateTime.Parse("11Sep2020"), DateTime.Parse("12Sep2020"));
+
+            var startDate = CheckDateFormat("Check-in Date");
+            var endDate = CheckDateFormat("Check-out Date");
+            reservation.GetCheapestBestRatedHotel(REWARD, startDate, endDate);
+        }
+
+        static DateTime CheckDateFormat(string type)
+        {
+            bool valid = false;
+            DateTime outDate;
+            string dateFormat = "ddMMMyyyy";
+            while (!valid)
+            {
+                Console.WriteLine("Enter " + type + " : (ddMMMyyyy)");
+                string date = Console.ReadLine();
+                if (DateTime.TryParseExact(date, dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out outDate))
+                {
+                    valid = true;
+                    return outDate;
+                }
+                else
+                    Console.WriteLine("Invalid date format");
+            }
+            return default;
         }
     }
 }
